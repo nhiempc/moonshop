f2502c11d6591eb02e125d2c1224c6574dd4d358
nhiempc <nhiem10596@gmail.com>
2022-08-01 03:54:30 +0700

    add some new routers

--- client/moonshop/src/routes/index.js
+++ client/moonshop/src/routes/index.js
@@ -4,14 +4,21 @@ import loadable from '@loadable/component';
 
 const HomePage = loadable(() => import('../pages/home/index'));
 const DetailPage = loadable(() => import('../pages/detail/index'));
-
+const CartPage = loadable(() => import('../pages/cart/index'));
+const LoginPage = loadable(() => import('../pages/login/index'));
+const RegisterPage = loadable(() => import('../pages/register/index'));
+const WishListPage = loadable(() => import('../pages/wishlist/index'));
 
 const RouteApp = () => {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage />}/>
+                <Route path="/" element={<HomePage/>}/>
                 <Route path="/details" element={<DetailPage/>}/>
+                <Route path="/cart" element={<CartPage/>}/>
+                <Route path="/wishlist" element={<WishListPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/register" element={<RegisterPage/>}/>
             </Routes>
         </BrowserRouter>
         
